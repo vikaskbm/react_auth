2432669f4ad0e5eb1fb48bda849540ec3e4fafee
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-25 02:06:31 +0530

    add firebase auth to signup user

---
+++ src/context/authContext.js
@@ -0,0 +1,25 @@
+import React, { useContext, useState } from "react";
+import { auth } from "../firebase";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function authProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+  const value = {
+    currentUser,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+      <h1>hell</h1>
+    </AuthContext.Provider>
+  );
+}
