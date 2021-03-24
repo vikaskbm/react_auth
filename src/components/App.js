e35eb5ee609f6b9963d629ed853eecec260cadbe
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-25 01:44:13 +0530

    setup signup.js file

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+function App() {
+  return <h2>Hello</h2>;
+}
+
+export default App;
---
+++ src/components/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Card, Form, Button } from "react-bootstrap";
+
+export default function Signup() {
+  return (
+    <>
+      <Card></Card>
+      <div className="w-100 text-center mt-2">Already have an account?</div>
+    </>
+  );
+}
